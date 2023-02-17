9fed37c96341c79b9771ecc271186096538b753d
Abdelrahman4040 <abdotaha105@gmail.com>
2023-02-17 00:44:22 -0800

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[] = "hello world\n";
+
+	puts(string);
+	return (0);
+}
