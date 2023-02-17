df2b340ee4a8c45f384bef92b856bbf8d5d4aa00
Abdelrahman4040 <abdotaha105@gmail.com>
2023-02-16 18:13:17 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <string.h>
-int main(void)
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);	
 
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
