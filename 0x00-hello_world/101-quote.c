27437213ca673a9bc1dee33fbb48f35c2847ff77
Abdelrahman4040 <abdotaha105@gmail.com>
2023-02-17 19:12:37 -0800

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
