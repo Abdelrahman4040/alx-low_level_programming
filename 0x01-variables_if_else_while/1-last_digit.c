a9eb4cdf849e9391097550574c1e5c3da136c27c
Abdelrahman4040 <abdotaha105@gmail.com>
2023-02-17 17:24:51 -0800

    commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,13 +9,12 @@
  */
 int main(void)
 {
-	int n;
-	int lastn;
-
-	lastn = n % 10;
+	int n, lastn;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
+	lastn = n % 10;
 
 	if (lastn < 6 && lastn > 0)
 	{
