584c7634686be6e20941b5826bcdb2d7f0dde0b9
Abdelrahman4040 <abdotaha105@gmail.com>
2023-02-24 16:02:59 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ * @size: number
+ * Return: 0
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	for (a = 1; a <= size; a++)
+	{
+		for (b = a; b < size; b++)
+		{
+		printf(" ");
+		}
+		for (c = 1; c <= a; c++)
+		{
+		printf("#");
+		}
+	printf("\n");
+	}
+}
