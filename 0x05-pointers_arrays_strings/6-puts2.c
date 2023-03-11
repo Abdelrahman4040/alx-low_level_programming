58cb2838d4aed21152399391fb42367e446d397d
Abdelrahman4040 <abdotaha105@gmail.com>
2023-03-11 03:43:07 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - Entry point
+ * @str: ptr to character
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int l = 0;
+	int i;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	for (i = 0; i < l - 1; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
