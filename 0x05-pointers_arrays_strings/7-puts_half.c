ea19d4dd8f19cd2441aaafec1d8f37df2978f841
Abdelrahman4040 <abdotaha105@gmail.com>
2023-03-11 04:45:20 -0800

    commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,10 +13,19 @@ void puts_half(char *str)
 	{
 		l++;
 	}
-	
+	if (l % 2 == 0)
+	{
 	for (i = l / 2; i <= l; i++)
 	{
 		_putchar(str[i]);
+	}
+	}
+	else
+	{
+	for (i = (l / 2) + 1; i <= l; i++)
+	{
+		_putchar(str[i]);
+	}
 	}
 	_putchar('\n');
 }
