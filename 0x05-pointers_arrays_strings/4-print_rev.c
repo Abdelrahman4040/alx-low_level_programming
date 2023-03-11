2f0cc73bccb9172c10d9a348062516eacc5978e4
Abdelrahman4040 <abdotaha105@gmail.com>
2023-03-10 20:42:12 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_re - Entry point
+ * @s: ptr to character
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	char *temp = s;
+	int i;
+
+	while (*temp != '\0')
+	{
+		temp++;
+	}
+	for (i = temp - s; i > 0; i--)
+	{
+		_putchar(*temp);
+		temp--;
+	}
+	_putchar('\n');
+}
